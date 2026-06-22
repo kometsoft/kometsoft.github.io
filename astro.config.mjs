1db65394f6b45ef88ea9568fdef0c8a3e7f8ed49
muhaizi <3365847+muhaizi@users.noreply.github.com>
2026-06-22 19:04:13 +0800

    feat : Update Astro and Tailwind CSS configuration; add Tailwind Vite plugin and upgrade Astro version

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from 'astro/config'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://kometsoft.com.my',
+  vite: {
+    plugins: [tailwindcss()],
+  },
 })
